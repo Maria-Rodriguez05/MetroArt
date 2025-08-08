3f6c4082dfd639bd4456a61665803a00b7e68e37
Maria-Rodriguez05 <maria-rodriguez@correo.unimet.edu.ve>
2025-08-07 21:17:04 -0400

    Pillow y validaciones
    Se agrego la libreria pillow para procesar el url de las imagenes en la primera opcion del menu, se uso el mismo codigo proporcionado en el apendice del enunciado del proyecto. tambien se incluyeron algunas validaciones que faltaban en las opciones 1 y 2 del menu

---
+++ MetroArt-main (1)/MetroArt-main/MetroArt-main/ProyectoPython/nacionalidad_test2.py
@@ -0,0 +1,340 @@
+#PARTE ANDRÉS SANTANA, NO SE HA IMPLEMENTADO AL MAIN 
+import requests
+def obt_ids():
+    url = "https://collectionapi.metmuseum.org/public/collection/v1/objects"
+    
+    respuesta = requests.get(url, timeout=100)
+    if respuesta.status_code == 200:
+        data = respuesta.json()
+        return data.get('objectIDs', [])
+    else:
+        print(f"Error: {respuesta.status_code}")
+        return []
+    
+def obt_obra_id(object_id):
+    url = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{object_id}"
+    respuesta = requests.get(url)
+    if respuesta.status_code == 200:
+        return respuesta.json()
+    else:
+        return None
+    
+def filtrar_nacionalidad(nacionalidad, max_resultados=5):
+    
+    resultados = []
+    
+    url_busqueda = f"https://collectionapi.metmuseum.org/public/collection/v1/search?artistOrCulture=true&q={nacionalidad}"
+    
+    respuesta = requests.get(url_busqueda, timeout=10)
+    
+    if respuesta.status_code == 200:
+        data = respuesta.json()
+        object_ids = data.get('objectIDs', [])
+        
+        print(f"Se encontraron {len(object_ids)} obras para la nacionalidad '{nacionalidad}'.")
+
+        for i, object_id in enumerate(object_ids):
+            if len(resultados) >= max_resultados:
+                break
+                
+            obra = obt_obra_id(object_id) 
+            if obra:
+                resultados.append({
+                    "titulo": obra.get("title"),
+                    "artista": obra.get("artistDisplayName"),
+                    "nacionalidad": obra.get("artistNationality"),
+                    "fecha": obra.get("objectDate"),
+                    "departamento": obra.get("department"),
+                    "url_imagen": obra.get("primaryImageSmall")
+                })
+                
+    else:
+        print(f"Error en la búsqueda: {respuesta.status_code}")
+        
+    return resultados
+    
+
+def mostrar_resultados(obras):
+    for obra in obras:
+        print(f"Título: {obra['titulo']}")
+        print(f"Artista: {obra['artista']}")
+        print(f"Nacionalidad: {obra['nacionalidad']}")
+        print(f"Fecha: {obra['fecha']}")
+        print(f"Departamento: {obra['departamento']}")
+        print(f"Imagen: {obra['url_imagen']}")
+        print("-" * 40)
+
+while True:
+    
+    op = str(input("""¿A cuál continente pertenece el país en el que el autor nació?
+1. África
+2. América 
+3. Asia
+4. Europa 
+5. Oceanía
+6. Salir
+----------->
+"""))
+
+    if op == "África" or op == "1" or op == "Africa":
+        op_af = str(input("""Elija la nacionalidad del autor (debe escribir la opción exactamente igual a como aparece en la lista)
+Argelia: Algerian
+Angola: Angolan
+Benín: Beninese
+Botsuana: Botswanan
+Burkina Faso: Burkinan
+Burundi: Burundian
+Cabo Verde: Cape Verdean
+Camerún: Cameroonian
+Chad: Chadian
+Comoras: Comoran
+Congo (Brazzaville): Congolese (Congo)
+Congo (RDC): Congolese (DRC)
+Costa de Marfil: Ivorian
+Djibouti: Djiboutian
+Egipto: Egyptian
+Eritrea: Eritrean
+Etiopía: Ethiopian
+Gabón: Gabonese
+Gambia: Gambian
+Ghana: Ghanaian
+Guinea: Guinean
+Guinea-Bissau: Citizen of Guinea-Bissau
+Guinea Ecuatorial: Equatorial Guinean
+Kenia: Kenyan
+Lesoto: Mosotho
+Liberia: Liberian
+Libia: Libyan
+Madagascar: Malagasy
+Malaui: Malawian
+Mali: Malian
+Marruecos: Moroccan
+Mauricio: Mauritian
+Mauritania: Mauritanian
+Mozambique: Mozambican
+Namibia: Namibian
+Níger: Nigerien
+Nigeria: Nigerian
+República Centroafricana: Central African
+Ruanda: Rwandan
+Santo Tomé y Príncipe: Sao Tomean
+Senegal: Senegalese
+Seychelles: Citizen of Seychelles
+Sierra Leona: Sierra Leonean
+Somalia: Somali
+Sudáfrica: South African
+Sudán: Sudanese
+Sudán del Sur: South Sudanese
+Suazilandia (Esuatini): Swazi
+Tanzania: Tanzanian
+Togo: Togolese
+Túnez: Tunisian
+Uganda: Ugandan
+Yibuti: Djiboutian
+Zambia: Zambian
+Zimbabue: Zimbabwean
+---------------------> 
+"""))
+        obras_encontradas = filtrar_nacionalidad(op_af)
+        mostrar_resultados(obras_encontradas)
+
+    elif op == "América" or op == "2" or op == "America":
+        op_am = str(input("""Elija la nacionalidad del autor (debe escribir la opción exactamente igual a como aparece en la lista)
+Anguila: Anguillan
+Antigua y Barbuda: Citizen of Antigua and Barbuda
+Argentina: Argentine
+Bahamas: Bahamian
+Barbados: Barbadian
+Belice: Belizean
+Bermudas: Bermudian
+Bolivia: Bolivian
+Brasil: Brazilian
+Canadá: Canadian
+Chile: Chilean
+Colombia: Colombian
+Costa Rica: Costa Rican
+Cuba: Cuban
+Dominica: Dominican
+Ecuador: Ecuadorean
+El Salvador: Salvadorean
+Estados Unidos: American
+Granada: Grenadian
+Groenlandia: Greenlandic
+Guatemala: Guatemalan
+Guayana: Guyanese
+Haití: Haitian
+Honduras: Honduran
+Islas Caimán: Cayman Islander
+Islas Turcas y Caicos: Turks and Caicos Islander
+Islas Vírgenes Británicas: British Virgin Islander
+Jamaica: Jamaican
+Martinica: Martiniquais
+México: Mexican
+Montserrat: Montserratian
+Nicaragua: Nicaraguan
+Panamá: Panamanian
+Paraguay: Paraguayan
+Perú: Peruvian
+Puerto Rico: Puerto Rican
+República Dominicana: Citizen of the Dominican Republic
+San Cristóbal y Nieves: Kittitian
+San Vicente y las Granadinas: Vincentian
+Santa Elena: St Helenian
+Santa Lucía: St Lucian
+Surinam: Surinamese
+Trinidad y Tobago: Trinidadian
+Uruguay: Uruguayan
+Venezuela: Venezuelan
+------------------>
+"""))
+        obras_encontradas = filtrar_nacionalidad(op_am)
+        mostrar_resultados(obras_encontradas)
+
+    elif op == "Asia" or op == "3":
+        op_as = str(input("""Elija la nacionalidad del autor (debe escribir la opción exactamente igual a como aparece en la lista)
+Afganistán: Afghan
+Arabia Saudí: Saudi Arabian
+Armenia: Armenian
+Azerbaiyán: Azerbaijani
+Baréin: Bahraini
+Bangladés: Bangladeshi
+Bhután: Bhutanese
+Brunéi: Bruneian
+Birmania (Myanmar): Burmese
+Camboya: Cambodian
+China: Chinese
+Corea del Norte: North Korean
+Corea del Sur: South Korean
+Emiratos Árabes Unidos: Emirati
+Filipinas: Filipino
+Georgia: Georgian
+Hong Kong: Hong Konger
+India: Indian
+Indonesia: Indonesian
+Irak: Iraqi
+Irán: Iranian
+Israel: Israeli
+Japón: Japanese
+Jordania: Jordanian
+Kazajistán: Kazakh
+Kirguistán: Kyrgyz
+Kuwait: Kuwaiti
+Laos: Lao
+Líbano: Lebanese
+Macao: Macanese
+Malasia: Malaysian
+Maldivas: Maldivian
+Mongolia: Mongolian
+Nepal: Nepalese
+Omán: Omani
+Pakistán: Pakistani
+Palestina: Palestinian
+Catar: Qatari
+Singapur: Singaporean
+Sri Lanka: Sri Lankan
+Siria: Syrian
+Taiwán: Taiwanese
+Tayikistán: Tajik
+Tailandia: Thai
+Timor Oriental: East Timorese
+Turkmenistán: Turkmen
+Turquía: Turkish
+Uzbekistán: Uzbek
+Vietnam: Vietnamese
+Yemen: Yemeni
+------------->
+"""))
+        obras_encontradas = filtrar_nacionalidad(op_as)
+        mostrar_resultados(obras_encontradas)
+    elif op == "Europa" or op == "4":
+        op_eu = str(input("""Elija la nacionalidad del autor (debe escribir la opción exactamente igual a como aparece en la lista)
+Alemania: German
+Andorra: Andorran
+Austria: Austrian
+Bielorrusia: Belarusian
+Bélgica: Belgian
+Bosnia y Herzegovina: Citizen of Bosnia and Herzegovina
+Bulgaria: Bulgarian
+Chipre: Cypriot
+Ciudad del Vaticano: Vatican citizen
+Croacia: Croatian
+Dinamarca: Danish
+Escocia: Scottish
+Eslovaquia: Slovak
+Eslovenia: Slovenian
+España: Spanish
+Estonia: Estonian
+Finlandia: Finnish
+Francia: French
+Gales: Welsh, Cymro, Cymraes
+Gibraltar: Gibraltarian
+Grecia: Greek
+Holanda: Dutch
+Hungría: Hungarian
+Inglaterra: English
+Irlanda: Irish
+Irlanda del Norte: Northern Irish
+Islandia: Icelandic
+Italia: Italian
+Kosovo: Kosovan
+Letonia: Latvian
+Liechtenstein: Liechtenstein citizen
+Lituania: Lithuanian
+Luxemburgo: Luxembourger
+Macedonia del Norte: Macedonian
+Malta: Maltese
+Moldavia: Moldovan
+Mónaco: Monegasque
+Montenegro: Montenegrin
+Noruega: Norwegian
+Polonia: Polish
+Portugal: Portuguese
+Reino Unido: British, Prydeinig
+República Checa: Czech
+Rumanía: Romanian
+Rusia: Russian
+San Marino: Sammarinese
+Serbia: Serbian
+Suecia: Swedish
+Suiza: Swiss
+Ucrania: Ukrainian
+-------------------->
+"""))
+        obras_encontradas = filtrar_nacionalidad(op_eu)
+        mostrar_resultados(obras_encontradas)
+    elif op == "Oceanía" or op =="5" or op =="Oceania":
+        op_oc = str(input("""Elija la nacionalidad del autor (debe escribir la opción exactamente igual a como aparece en la lista)
+Australia: Australian
+Fiyi: Fijian
+Islas Cook: Cook Islander
+Islas Feroe: Faroese
+Islas Marshall: Marshallese
+Islas Pitcairn: Pitcairn Islander
+Islas Salomón: Solomon Islander
+Kiribati: Citizen of Kiribati
+Micronesia: Micronesian
+Nauru: Nauruan
+Niue: Niuean
+Nueva Zelanda: New Zealander
+Palaos: Palauan
+Papúa Nueva Guinea: Papua New Guinean
+Samoa: Samoan
+Tokelau: Tokelauan
+Tonga: Tongan
+Tristán de Acuña: Tristanian
+Tuvalu: Tuvaluan
+Vanuatu: Citizen of Vanuatu
+Wallis y Futuna: Wallisian
+__---------------------->
+"""))
+        obras_encontradas = filtrar_nacionalidad(op_oc)
+        mostrar_resultados(obras_encontradas)
+    elif op == "Salir" or op == "6":
+        print("Ha salido de la opción")
+        break
+                    
+        
+nacionalidad = input("Ingresa la nacionalidad del autor (en inglés):")
+obras_encontradas = filtrar_nacionalidad(nacionalidad)
+
+mostrar_resultados(obras_encontradas)
---
+++ MetroArt-main (1)/MetroArt-main/MetroArt-main/README.md
@@ -0,0 +1,8 @@
+# MetroArt
+
+Integrantes:
+Maria Rodriguez
+Andres Santana
+Ines Milano
+
+proyecto algoritmo metroArt
